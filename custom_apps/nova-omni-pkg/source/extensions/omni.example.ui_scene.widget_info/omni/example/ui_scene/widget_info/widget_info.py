ad75c8d313dab6bf0f853c3f6f282f2859b9ade7
andballard <147875562+andballard@users.noreply.github.com>
2024-01-10 09:21:32 -0600

    updated widget

--- custom_apps/nova-omni-pkg/source/extensions/omni.example.ui_scene.widget_info/omni/example/ui_scene/widget_info/widget_info.py
+++ custom_apps/nova-omni-pkg/source/extensions/omni.example.ui_scene.widget_info/omni/example/ui_scene/widget_info/widget_info.py
@@ -1,6 +1,9 @@
 import omni.ui as ui
 import omni.usd
 import carb
+import math
+import time
+from threading import Timer
 from omni.ui import color as cl
 from pxr import Usd, Tf, UsdGeom, UsdShade
 
@@ -11,7 +14,10 @@ class InfoWidgetProvider():
     """
 
     def __init__(self):
+        self.start = time.time()
+        self.end = time.time()
         self.is_pinned = False
+        self.history = [0]
         self.usd_context = omni.usd.get_context()
         self.stage = self.usd_context.get_stage()
         self.stage_event_sub = omni.usd.get_context().get_stage_event_stream().create_subscription_to_pop(
@@ -72,11 +78,12 @@ class InfoWidgetProvider():
         with self._ui_header_frame:
             with ui.Stack(ui.Direction.RIGHT_TO_LEFT):
                 ui.Button(text=" x ", width=0, height=0, clicked_fn=lambda *args: self.close())
-                ui.Button(text="Unpin" if self.is_pinned else "Pin", width=0, height=0, clicked_fn=lambda *args: self.togglePin())
+                ui.Label("IoT Data", name="prop_label", word_wrap=True, width=390, alignment=ui.Alignment.CENTER, style={"font":"${fonts}/OpenSans-SemiBold.ttf", "font_size": 40.0})
+                #ui.Button(text="Unpin" if self.is_pinned else "Pin", width=0, height=0, clicked_fn=lambda *args: self.togglePin())
                 ui.Spacer()
 
     def widget_header(self, style={"margin":2}):
-        with ui.VStack(height=0,style=style):
+        with ui.VStack(height=10,style=style):
             self._ui_header_frame = ui.Frame(name="ui_widget_header_frame")
             self._widget_header()
 
@@ -91,9 +98,9 @@ class InfoWidgetProvider():
         # if not self._placer.visible:
         #     self._placer.visible = True
 
-        self._placer = ui.Placer(visible = False, draggable=True, offset_x=100, offset_y = 100, height = 50)
+        self._placer = ui.Placer(visible = False, draggable=True, offset_x=100, offset_y = 100)
         with self._placer:
-            with ui.ZStack(height=50, width=500, style={"margin":2}):
+            with ui.ZStack(width=450, height=10, style={"margin":2}):
                 ui.Rectangle(
                     style={
                         # "background_color": cl(0.2),
@@ -104,27 +111,15 @@ class InfoWidgetProvider():
                     }
                 )
 
-                with ui.VStack(height=50):
+                with ui.VStack(height=10):
                     self.widget_header()
                     self._render_body()
 
     def _render_body(self):
-        # def on_mouse_wheel(self, x, y, modifier):
-        #     if modifier == carb.input.KEYBOARD_MODIFIER_FLAG_SHIFT:
-        #         # scroll horizontally
-        #         scroll_x = max(0, min(self._scrollingFrame.scroll_x - x, self._scrollingFrame.scroll_x_max))
-        #         self._scrollingFrame.scroll_here(scroll_x, self._scrollingFrame.scroll_y)
-        #     else:
-        #         # scroll vertically
-        #         scroll_y = max(0, min(self._scrollingFrame.scroll_y - y, self._scrollingFrame.scroll_y_max))
-        #         self._scrollingFrame.scroll_here(self._scrollingFrame.scroll_x, scroll_y)
-        # self.viewport_window.frame.set_mouse_wheel_fn(on_mouse_wheel)
-
-
-        self._zstack = ui.ZStack(height = 50)
+        #self._zstack = ui.ZStack(height = 50)
         self._scrollingFrame = ui.ScrollingFrame(
                 style={
-                    "background_color": cl("#000000bf"),
+                    "background_color": cl("#000670bf"),
                     "border_width": 10,
                     "border_color": cl(0.7),
                     "border_radius": 4,
@@ -134,53 +129,36 @@ class InfoWidgetProvider():
                     "scrollbar_background_color": cl(0.3),
                 }
             )
-        with self._zstack:
-            self._scrollingFrame
-                # with ui.Frame():
-                #     # Create a grid layout manager
-                #     with ui.Grid(ui.Direction.TOP_TO_BOTTOM, selected=True):
-                #     # Add some labels to the grid
-                #         for i in range(40):
-                #             with ui.HStack(width=0):
-                #                 self._J1_label_name = ui.Label(f"J{i}", name="prop_label", word_wrap=True, width=100)
-            # self._rectangle = ui.Rectangle(
-            #     width=800,
-            #     height=800,
-            #     style={
-            #         "background_color": cl("#000000bf"),
-            #         "border_color": cl(0.7),
-            #         "border_width": 0,
-            #         "border_radius": 4,
-            #     }
-            # )
+        self._scrollingFrame.height = ui.Length(300)
+        #with self._zstack:
+        with self._scrollingFrame:
+            # with ui.Frame():
+            #     # Create a grid layout manager
+            #     with ui.Grid(ui.Direction.TOP_TO_BOTTOM, selected=True):
+            #     # Add some labels to the grid
+            #         for i in range(40):
+            #             with ui.HStack(width=0):
+            #                 self._J1_label_name = ui.Label(f"J{i}", name="prop_label", word_wrap=True, width=100)
+        # self._rectangle = ui.Rectangle(
+        #     width=800,
+        #     height=800,
+        #     style={
+        #         "background_color": cl("#000000bf"),
+        #         "border_color": cl(0.7),
+        #         "border_width": 0,
+        #         "border_radius": 4,
+        #     }
+        # )
 
-            self._mainStack = ui.VStack(style={"font_size": 28,"margin": 8}, height = 50)
-            with self._mainStack:
-                # ui.Button(text=" x ", width=0, height=0, clicked_fn=lambda *args: self.close(), alignment=ui.Alignment.RIGHT)
-                ui.Spacer(height=2)
-                self._name_label = ui.Label("", height=0, alignment=ui.Alignment.CENTER)
-                # sparkline = ui.Sparkline(parent=window, name='sparkline', data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                # data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-                # with ui.HStack():
-                #     ui.Spacer(width=200) # align the sparklines with the scrollable frame
-                #     for value in data:
-                #         with ui.VStack():
-                #             ui.Sparkline(
-                #                 data=[value],
-                #                 style={
-                #                     "sparkline_type": 0,
-                #                     "sparkline_color": cl(0.9),
-                #                     "sparkline_width": 2,
-                #                     "marker_color": cl(0.9),
-                #                     "marker_size": 4,
-                #                     "show_markers": True,
-                #                 },
-                #             )
-                #         ui.Label(str(value), alignment=ui.Alignment.CENTER)
-
+            with ui.VStack(style={"font_size": 28,"margin": 8}, height = 50):
                 with ui.HStack(width=0):
                     self._J0_label_name = ui.Label("J0", name="prop_label", word_wrap=True, width=100)
                     self._J0_label_val = ui.Label("", name="prop_value")
+                    set_data = []
+                    for i in range(360):
+                        set_data.append(math.cos(math.radians(i)))
+
+                    self._plot = ui.Plot(ui.Type.LINE, -1.0, 1.0, *set_data, width=180, height=100, style={"color": cl.red})
                 with ui.HStack(width=0):
                     self._J1_label_name = ui.Label("J1", name="prop_label", word_wrap=True, width=100)
                     self._J1_label_val = ui.Label("", name="prop_value")
@@ -204,26 +182,16 @@ class InfoWidgetProvider():
                     self._pressure_val = ui.Label("", name="prop_value")
                 with ui.HStack(width=0):
                     self._alert_name = ui.Label("alert", name="prop_label", word_wrap=True, width=100)
-                    self._alert_val = ui.Label("", name="prop_value")
-
-        self._scrollingFrame.add_child(self._mainStack)
-        # self._scrollingFrame.scroll_only_window_hovered = False
-        # self._scrollingFrame.set_mouse_wheel_fn(on_mouse_wheel)
-        # self._scrollingFrame.scroll_here(0, 1)
-
-        # self._scrollingFrame.set_scroll_y_changed_fn(on_mouse_wheel)
-
-        # Set the functions to be called when the scroll bars are moved
-        self._scrollingFrame.set_scroll_x_changed_fn(lambda x: print(f"Horizontal scroll bar moved to {x}"))
-        self._scrollingFrame.set_scroll_y_changed_fn(lambda y: print(f"Vertical scroll bar moved to {y}"))
+                    self._alert_val = ui.Label("alert", name="prop_value", word_wrap=True, width=100)
 
         self.listener = Tf.Notice.Register(Usd.Notice.ObjectsChanged, self._on_objects_changed, self.stage)
 
-        # Register the function to be called when a mouse wheel event occurs
-        # carb.events.register_listener(self.handle_mouse_wheel, "carb.input.mouse.wheel")
-
-        # self._scrollingFrame = ui.ScrollingFrame
-        # self._scrollingFrame.set_mouse_wheel_fn(on_mouse_wheel)
+    def addlabel(self):
+        self._hs1 = ui.HStack(width=0)
+        with self._hs1:
+            self._J0_label_name = ui.Label("J0", name="prop_label", word_wrap=True, width=100)
+            self._J0_label_val = ui.Label("", name="prop_value")
+        return self._hs1
 
     def _on_objects_changed(self, notice, stage):
         updated_objects = []
@@ -248,6 +216,18 @@ class InfoWidgetProvider():
                 # path = str(o.pathString)
                 if 'j0' in o.pathString:
                     self._J0_label_val.text = val
+                    # if len(self.history) < 500:
+                    #     self.history.append(float(val))
+                    # else:
+                    #     self.history.pop(0)
+                    #     self.history.append(float(val))
+
+                    # self.end = time.time()
+                    # if self.end - self.start >= 10:
+                    #     self._plot.set_data(valueList = *self.history)
+                    #     print(f"StartTime: {str(self.start)}")
+                    #     print(f"EndTime: {str(self.end)}")
+                    #    self.start = self.end
                 elif 'j1' in o.pathString:
                     self._J1_label_val.text = val
                 elif 'j2' in o.pathString:
